685ac9b934fba7c50ffa2b6ac0aa9c7841159393
Taliya05 <dinulya05@yandex.ru>
2022-08-27 20:42:57 +0300

    cfhj

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-let isRoots = document.getElementsByClassName('isRoots')[0];
-let isnotRoots = document.getElementsByClassName('isnotRoots')[0];
-let bottomend = document.getElementsByClassName('bottomend')[0];
+let isRoots = document.getElementsByClassName("isRoots")[0];
+let isnotRoots = document.getElementsByClassName("isnotRoots")[0];
+let bottomend = document.getElementsByClassName("bottomend")[0];
 
 let btn_calc = document.getElementById("btn_calc");
 
 setVisible(isRoots, "none");
 setVisible(isnotRoots, "none");
 
-function setVisible(cl, vall) { 
-  cl.style.display = vall; 
+function setVisible(cl, vall) {
+  cl.style.display = vall;
 }
 
 btn_calc.onclick = function calc() {
@@ -19,15 +19,14 @@ btn_calc.onclick = function calc() {
   let x1 = document.getElementById("x1").value;
   let x2 = document.getElementById("x2").value;
 
-  let d = b * b - (4 * a * c);
+  let d = b * b - 4 * a * c;
 
   setVisible(bottomend, "none");
 
   if (d < 0) {
     setVisible(isRoots, "none");
     setVisible(isnotRoots, "block");
-  } 
-  else {
+  } else {
     setVisible(isRoots, "block");
     setVisible(isnotRoots, "none");
 
@@ -36,11 +35,5 @@ btn_calc.onclick = function calc() {
 
     document.getElementById("x1").value = x1;
     document.getElementById("x2").value = x2;
-
   }
-
-
- }
-
-
-
+};
